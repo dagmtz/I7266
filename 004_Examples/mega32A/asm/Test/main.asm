1439e63673fb92eb83a284a1905db26a2a67fc9d
dagmtz <dagmtzs@gmail.com>
2024-09-21 15:49:05 -0600

    Minor updates

--- 004_Examples/mega32A/asm/Test/main.asm
+++ 004_Examples/mega32A/asm/Test/main.asm
@@ -1,8 +1,12 @@
 ;
-; 000_Playground.asm
+; 	File:                   main.c
+;   Author:                 Daniel Martinez
+;                           dagmtzs@gmail.com
 ;
-; Created: 21/09/2024 10:27:24 a. m.
-; Author : dagmtz
+;   Created: 				21/09/2024 10:27:24 a. m.
+;   Target:                 ATmega32A
+;   Description:            This program is a "Hello World!" implementation for the 
+;                           ATmega32A. It turns on an LED connected to PORTD7.
 ;
 
 ;.include "m32Adef.inc"
@@ -10,7 +14,6 @@
 .equ	DDRD	= 0x11
 .equ	PORTD	= 0x12
 
-; Replace with your application code
 Start:
 		ldi	R16, 0x80
 		out DDRD, R16
